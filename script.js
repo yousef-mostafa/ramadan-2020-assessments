fbc7e64c27ee7e200e00f13d0bc6aa70e26667b0
Yousef Mostafa <yousef.m.saber2001@gmail.com>
2022-08-22 13:42:33 +0200

    task 3

--- script.js
+++ script.js
@@ -1,57 +1,94 @@
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
-
+  let vote; 
 // func to load video posts from db
-    function loadVideos() {
-      // get all request from  '/video-request' and list them
-      fetch("http://localhost:7777/video-request")
-        .then((blob) => blob.json())
-        .then((data) => {
-          let videoList = document.querySelector("#listOfRequests");
-          videoList.innerHTML = "";
-          data.forEach((element) => {
-            let submit_date = new Date (element.submit_date)
-            let videoItem = document.createElement("div");
-            videoItem.classList.add("card", "mb-3");
-            videoItem.innerHTML = `
-                <div class="card-body d-flex justify-content-between flex-row">
-                <div class="d-flex flex-column">
-                  <h3>${element.topic_title}</h3>
-                  <p class="text-muted mb-2">${element.topic_details}</p>
-                  <p class="mb-0 text-muted">
-                  ${element.expected_result && `<strong>Expected results: </strong> ${element.expected_result}`}
-                  </p>
-                </div>
-                <div class="d-flex flex-column text-center">
-                  <a class="btn btn-link">🔺</a>
-                  <h3>${element.votes.ups - element.votes.downs}</h3>
-                  <a class="btn btn-link">🔻</a>
+  function loadVideo(videoEle) {
+          let submit_date = new Date (videoEle.submit_date)
+          let videoItem = document.createElement("div");
+          videoItem.classList.add("card", "mb-3");
+          videoItem.innerHTML = `
+              <div class="card-body d-flex justify-content-between flex-row">
+              <div class="d-flex flex-column">
+                <h3>${videoEle.topic_title}</h3>
+                <p class="text-muted mb-2">${videoEle.topic_details}</p>
+                <p class="mb-0 text-muted">
+                ${videoEle.expected_result && `<strong>Expected results: </strong> ${videoEle.expected_result}`}
+                </p>
+              </div>
+              <div class="d-flex flex-column text-center">
+                <a id="vote_ups_${videoEle._id}" class="btn btn-link">🔺</a>
+                <h3 id="vote_${videoEle._id}">${videoEle.votes.ups - videoEle.votes.downs}</h3>
+                <a id="vote_downs_${videoEle._id}" class="btn btn-link">🔻</a>
+              </div>
+            </div>
+            <div class="card-footer d-flex flex-row justify-content-between">
+              <div>
+                <span class="text-info">${videoEle.status}</span>
+                &bullet; added by <strong>${videoEle.author_name}</strong> on
+                <strong>${days[submit_date.getDay()]} ${
+            months[submit_date.getMonth()]
+          } ${submit_date.getDate()} ${submit_date.getFullYear()} at ${submit_date.getHours()}:${submit_date.getMinutes()}</strong> 
+              </div>
+              <div
+                class="d-flex justify-content-center flex-column 408ml-auto mr-2"
+              >
+                <div class="badge badge-success">
+                  ${videoEle.target_level}
                 </div>
               </div>
-              <div class="card-footer d-flex flex-row justify-content-between">
-                <div>
-                  <span class="text-info">${element.status}</span>
-                  &bullet; added by <strong>${element.author_name}</strong> on
-                  <strong>${days[submit_date.getDay()]} ${
-              months[submit_date.getMonth()]
-            } ${submit_date.getDate()} ${submit_date.getFullYear()} at ${submit_date.getHours()}:${submit_date.getMinutes()}</strong> 
-                </div>
-                <div
-                  class="d-flex justify-content-center flex-column 408ml-auto mr-2"
-                >
-                  <div class="badge badge-success">
-                    ${element.target_level}
-                  </div>
-                </div>
-              </div>
-                `;
-            videoList.append(videoItem);
-          });
-        });
-    }
+            </div>
+              `;
+          return videoItem;
+  }
+
+function getAllVideos() {
+  // get all request from  '/video-request' and list them
+    fetch("http://localhost:7777/video-request")
+      .then((bolb) => bolb.json())
+      .then((data) => {
+        let videoList = document.querySelector("#listOfRequests");
+        videoList.innerHTML = "";
+        data.forEach((videoEle) => {
+          videoList.append(loadVideo(videoEle));
+          let vote_up = document.getElementById(`vote_ups_${videoEle._id}`);
+          let vote_down = document.getElementById(`vote_downs_${videoEle._id}`);
+          let vote
+          vote_up.addEventListener("click" , () =>{
+            vote = document.querySelector(`#vote_${videoEle._id}`);
+            fetch('http://localhost:7777/video-request/vote',{
+              method: 'PUT',
+              headers: {'content-Type' :'application/json'},
+              body:JSON.stringify({
+                id: videoEle._id,
+                vote_type: "ups"
+              })
+            }).then((bolb) => bolb.json())
+            .then((data) => {
+              vote.innerHTML = `${data.ups - data.downs}`
+            })
+          })
+
+          vote_down.addEventListener("click" , () =>{
+            vote = document.querySelector(`#vote_${videoEle._id}`);
+            fetch('http://localhost:7777/video-request/vote',{
+              method: 'PUT',
+              headers: {'content-Type' :'application/json'},
+              body:JSON.stringify({
+                id: videoEle._id,
+                vote_type: "downs"
+              })
+            }).then((bolb) => bolb.json())
+            .then((data) => {
+              vote.innerHTML = `${data.ups - data.downs }`
+            })
+          })
+        })
+      })
+  }
 document.addEventListener("DOMContentLoaded", () => {
-    let formVideo = document.querySelector("#submitVideo");
-  loadVideos();
+  let formVideo = document.querySelector("#submitVideo");
+  getAllVideos();
+  
   // submit request
   formVideo.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -59,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:7777/video-request", {
       method: "POST",
       body: dataForm,
-    }).then(() => loadVideos());
+    }).then(() => getAllVideos());
   });
 
 });
